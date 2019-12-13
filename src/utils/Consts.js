fcf3e55c8f44b1b89f95bd22e34ac1ddad4fe762
ingogbe <ingo_eyng_1992@hotmail.com>
2019-12-12 21:32:37 -0300

    update production server url

--- src/utils/Consts.js
+++ src/utils/Consts.js
@@ -1,4 +1,4 @@
 export const devMode = process.env.NODE_ENV === 'development' ? true : false;
 
 export const drawerWidth = 240;
-export const host = devMode ? "http://localhost:5000" : "https://app.evah.io";
+export const host = devMode ? "http://localhost:5000" : "https://desafio-repassa.firebaseapp.com";
